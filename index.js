628472887b1d442ee982c3ba626ea9e998a66066
YasserEsam <yaaser10esam1010@gmail.com>
2024-08-26 23:48:59 +0300

    pass the test with honer and respect

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 class Formatter {
-  //add static methods here
+
+  static capitalize( str ) {
+    return str.charAt( 0 ).toUpperCase() + str.slice( 1 )
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^a-zA-Z0-9-' ]/g, '');
+  }
+  static titleize(str) {
+    const lowercaseWords = ['and', 'for', 'of', 'at', 'by', 'from', 'to', 'a', 'an', 'the', 'but'];
+
+    return str.split(' ').map((word, index) => {
+      if (index === 0 || !lowercaseWords.includes(word)) {
+        return Formatter.capitalize(word);
+      } else {
+        return word;
+      }
+    }).join(' ');
+  }
 }
